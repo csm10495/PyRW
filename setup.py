a5059e909ed8259dd6750d710676b1d5de9d95f9
csm10495 <csm10495@gmail.com>
2018-07-09 20:34:44 -0700

    versioning

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     author='csm10495',
     author_email='csm10495@gmail.com',
     url='http://github.com/csm10495/pyrw',
-    version='0.2d',
+    version='0.3a',
     packages=['pyrw'],
     license='MIT License',
     python_requires='>=2.7',
